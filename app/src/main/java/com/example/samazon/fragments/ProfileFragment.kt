b4accd7bd751f0dea2fde7b5ff7197f2201dbcef
GiorgiZan <giorgizandarashvili7@gmail.com>
2022-01-19 21:14:38 +0400

    davamte yvlefaeri proeqti dasrulebuli iqneba 20 ianvars 21:00-ze

--- app/src/main/java/com/example/samazon/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/samazon/fragments/ProfileFragment.kt
@@ -5,20 +5,37 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import com.example.samazon.ChangePasswordActivity
 import com.example.samazon.LoginActivity
 import com.example.samazon.R
+import com.example.samazon.data.UserInfo
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+    private lateinit var userNameTextView : TextView
+    private val auth = FirebaseAuth.getInstance()
+    private val db = FirebaseDatabase.getInstance().getReference("Users")
 
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        userNameTextView = view.findViewById(R.id.userName)
 
+
+        db.child(auth.currentUser?.uid!!).get().addOnSuccessListener {
+            if (it.exists()) {
+                val userName = it.child("userName").value
+
+
+                userNameTextView.text = "$userName"
+            }
+        }
     }
 }
